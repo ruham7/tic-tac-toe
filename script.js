466e484ccc7bad27dda7e3c267de3c11906c9f26
Ruhama Demeke <ruhama.demeke@gmail.com>
2022-08-09 15:22:45 +0300

    js first

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer');
+
+let board = ['','','','','','','','',''];
+let currentPlayer = 'X';
+let isGameActive = true;
+
+
+      resetButton.addEventListener('click', resetBoard);
+}) 
