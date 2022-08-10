63e0d06c5bf2e38c644e750377b1f1b04b9774e9
Ruhama Demeke <ruhama.demeke@gmail.com>
2022-08-10 15:41:39 +0300

    more js

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let board = ['','','','','','','','',''];
 let currentPlayer = 'X';
 let isGameActive = true;
 
+const PlayerXWon = 'Player X won';
+const PlayerOWon = 'Player O won';
+const Tie = 'Tie';
 
+const winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+tiles.forEach((title, index) => {
+      tiles.addEventListener('click',()=> userActon(tile, index));
+});
       resetButton.addEventListener('click', resetBoard);
 }) 
